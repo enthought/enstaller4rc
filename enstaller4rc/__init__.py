809c65fe6aabfe2dded89c102f9ad0d4017143ad
David Cournapeau <cournape@gmail.com>
2016-04-07 09:14:46 +0100

    FEAT: add search_order to get where to look for enstaller4rc.

--- enstaller4rc/__init__.py
+++ enstaller4rc/__init__.py
@@ -4,3 +4,6 @@ from ._placeholders import (
     APITokenAuth, ProxyConfiguration, UserPasswordAuth  # noqa
 )
 from .config import Configuration  # noqa
+from .utils import search_order
+
+ENSTALLER4RC_FILENAME = ".enstaller4rc"
